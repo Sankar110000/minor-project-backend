53e8302c5ca31f7e6d527a67f1db9c8228d5aa42
sbsolute-sankar <skaibalya2005@gmail.com>
2025-11-03 11:18:11 +0530

    removed unnecssary files

--- routes/class.routes.js
+++ routes/class.routes.js
@@ -2,12 +2,14 @@ const {
   createNewClass,
   getCurrentClass,
   getPrevoiusClass,
+  getAllClasses,
 } = require("../controllers/class.controller");
 
 const classRouter = require("express").Router();
 
 classRouter.post("/create", createNewClass);
 classRouter.post("/getCurrClass", getCurrentClass);
-classRouter.post("/getAllClasses", getPrevoiusClass);
+classRouter.post("/getPrevoiusClass", getPrevoiusClass);
+classRouter.post("/getAllClasses", getAllClasses);
 
 module.exports = classRouter;
